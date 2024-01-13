7c0f712dca51ee2e74f974fc17a25ec395b79524
Artyom Burnaev <grazenone@gmail.com>
2024-01-13 16:30:36 +0300

    ok

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -18,15 +18,17 @@ public class AnimalsCage {
     private Timer timer;
 
 
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
     public Timer getTimer() {
+
         return timer;
     }
 }
